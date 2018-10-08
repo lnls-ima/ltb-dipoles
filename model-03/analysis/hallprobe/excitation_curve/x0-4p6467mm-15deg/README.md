2e0ea1815d49be825102e39a2cc5d6c54c0fcd69
ximenes <xresende@gmail.com>
2018-10-08 09:20:39 -0300

    Add fmap and hallsensor analysis for neg polarity

---
+++ model-03/analysis/hallprobe/excitation_curve/x0-4p6467mm-15deg/README.md
@@ -0,0 +1,1 @@
+TBD-01 is an inverted dipole. TBD-01-pos is its analysis with inverted polarity and x0
--- model-03/analysis/hallprobe/excitation_curve/x0-4p6467mm-15deg/run-all.bash
+++ model-03/analysis/hallprobe/excitation_curve/x0-4p6467mm-15deg/run-all.bash
@@ -1,36 +1,38 @@
 #!/usr/bin/env bash
 
 
-cp TBD-01/0025p362A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0025p362A-positive.txt
-cp TBD-01/0050p724A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0050p724A-positive.txt
-cp TBD-01/0076p086A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0076p086A-positive.txt
-cp TBD-01/0101p449A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0101p449A-positive.txt
-cp TBD-01/0126p811A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0126p811A-positive.txt
-cp TBD-01/0152p173A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0152p173A-positive.txt
-cp TBD-01/0177p535A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0177p535A-positive.txt
-cp TBD-01/0202p898A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0202p898A-positive.txt
-cp TBD-01/0228p261A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0228p261A-positive.txt
-cp TBD-01/0240p941A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0240p941A-positive.txt
-cp TBD-01/0253p622A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0253p622A-positive.txt
-cp TBD-01/0266p303A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0266p303A-positive.txt
-cp TBD-01/0278p984A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0278p984A-positive.txt
-cp TBD-01/0304p346A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0304p346A-positive.txt
-cp TBD-01/0329p709A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0329p709A-positive.txt
-cp TBD-01/0025p362A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0025p362A-negative.txt
-cp TBD-01/0050p724A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0050p724A-negative.txt
-cp TBD-01/0076p086A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0076p086A-negative.txt
-cp TBD-01/0101p449A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0101p449A-negative.txt
-cp TBD-01/0126p811A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0126p811A-negative.txt
-cp TBD-01/0152p173A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0152p173A-negative.txt
-cp TBD-01/0177p535A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0177p535A-negative.txt
-cp TBD-01/0202p898A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0202p898A-negative.txt
-cp TBD-01/0228p261A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0228p261A-negative.txt
-cp TBD-01/0240p941A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0240p941A-negative.txt
-cp TBD-01/0253p622A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0253p622A-negative.txt
-cp TBD-01/0266p303A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0266p303A-negative.txt
-cp TBD-01/0278p984A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0278p984A-negative.txt
-cp TBD-01/0304p346A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0304p346A-negative.txt
-cp TBD-01/0329p709A/z-negative/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-01-0329p709A-negative.txt
+cd TBD-01-pos/0025p362A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0050p724A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0076p086A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0101p449A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0126p811A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0152p173A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0177p535A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0202p898A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0228p261A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0240p941A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0253p622A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0266p303A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0278p984A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0304p346A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0329p709A/z-positive/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0025p362A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0050p724A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0076p086A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0101p449A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0126p811A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0152p173A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0177p535A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0202p898A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0228p261A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0240p941A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0253p622A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0266p303A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0278p984A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0304p346A/z-negative/; fac-fma-analysis.py run; cd ../../../
+cd TBD-01-pos/0329p709A/z-negative/; fac-fma-analysis.py run; cd ../../../
+
+exit
 
 
 cp TBD-02/0025p362A/z-positive/analysis.txt /home/fac_files/lnls-fac/MatlabMiddleLayer/Release/machine/SIRIUS/TB.V03.01/models/dipoles/tbd-02-0025p362A-positive.txt
